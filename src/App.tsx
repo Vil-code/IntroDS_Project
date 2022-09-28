c94a0d4ca1e6947f9995f6d537a047278ff3ceda
Vilhelmi Rintanen <vilhelmi.rintanen@sanoma.com>
2022-09-28 16:08:03 +0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,30 @@ import './App.css';
 
 function App() {
 
-const [genres, setGenres] = useState<[String]>([""]);
-const [seenAnime, setSeenAnime] = useState<[String]>([""]);
+const [genres, setGenres] = useState<string[]>([""]);
+const [seenAnime, setSeenAnime] = useState<string[]>([""]);
+
+type genre = {
+  label: string
+}
+
+const genreChoices: genre[] = [
+  {
+    label: 'Shounen'
+  },{
+    label: 'Comedy'
+  },{
+    label: 'Romance'
+  },{
+    label: 'Sports'
+  }
+]
 
   return (
     <div className="App">
-
+      <ul> 
+        <li></li>
+      </ul>
     </div>
   );
 }
