3444e9787bc90f7b3d4356ebafa48130566dac7e
Vilhelmi Rintanen <vilhelmi.rintanen@sanoma.com>
2022-10-25 22:30:39 +0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   const getRecommendations = async (genres: string) => {
     try {
       const request = await axios.post(
-        "https://anime-recommender-intro-ds.herokuapp.com/recommendations",
+        "http://127.0.0.1:8000/recommendations",
         genres
       )
       console.log("request data is:" + request.data.data.Page.media[0].title.romaji)
