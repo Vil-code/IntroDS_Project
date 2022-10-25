6e26c767ae39fffa6d083804fd7b2a9723b9cb3d
Vilhelmi Rintanen <vilhelmi.rintanen@sanoma.com>
2022-10-25 22:42:14 +0300

    c

--- query.py
+++ query.py
@@ -52,5 +52,4 @@ def recommendations():
 @cross_origin()    
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
